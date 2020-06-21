27e0259777ce581899c9fa38c0e2733419f16e72
rubym <ruby.montes.rosas.gmail.com>
2020-06-21 14:15:31 -0500

    crea archivos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" >
+    <Header/>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <Categories/>
+          <Types/>
+        </div>
+        <div class="col-md-8">
+          <router-view></router-view>
+        </div>
+      </div> 
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import  Header from './components/Header'
+import  Categories from './components/Categories'
+import  Types from './components/Types'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Categories,
+    Types
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Geraldi Ruby Montes Rosas 9°A</h1>
+    </div>
+</template>
+
